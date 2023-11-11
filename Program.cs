17f553346e75b197f8035e3d74e5914125d17c4b
Zeldris20 <christianclapson@gmail.com>
2023-11-11 08:17:44 -0600

    PasswordChecker

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Password Strength Checker");
+
+        Console.Write("Enter your password: ");
+        string password = Console.ReadLine();
+
+        var passwordHasher = new PasswordHasher<string>();
+        var passwordHash = passwordHasher.HashPassword(null, password);
+
+        Console.WriteLine($"Password Hash: {passwordHash}");
+
+        var passwordVerificationResult = passwordHasher.VerifyHashedPassword(null, passwordHash, password);
+
+        Console.WriteLine($"Password Verification Result: {passwordVerificationResult}");
+    }
+}
